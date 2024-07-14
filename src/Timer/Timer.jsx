e58f1863c5fe4668d11f7aae9bb181536ffdc01f
VuktorTS <skalina222@gmail.com>
2024-07-14 23:58:51 +0300

    add TimeDisplay components

--- src/Timer/Timer.jsx
+++ src/Timer/Timer.jsx
@@ -1,20 +1,11 @@
 import Button from "../Button/Button";
-import { Colon, Number, Part, Parts, TimerSection, Unit } from "./Timer.styled";
+import TimeDisplay from "../TimeDisplay/TimeDisplay";
+import { TimerSection } from "./Timer.styled";
 
 function Timer() {
   return (
     <TimerSection>
-      <Parts>
-        <Part>
-          <Number>05</Number>
-          <Unit>minutes</Unit>
-        </Part>
-        <Colon>:</Colon>
-        <Part>
-          <Number>00</Number>
-          <Unit>seconds</Unit>
-        </Part>
-      </Parts>
+      <TimeDisplay />
       <Button title="Play" icon="play" />
     </TimerSection>
   );
