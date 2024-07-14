fd74c6ab5c98aa0664680bbb798ca6427d90de98
VuktorTS <skalina222@gmail.com>
2024-07-14 17:35:41 +0300

    initial commit

---
+++ src/TimerManager/TimerManager.jsx
@@ -0,0 +1,12 @@
+import Timer from "../Timer/Timer";
+import { Timers } from "./TimerManager.styled";
+
+function TimerManager() {
+  return (
+    <Timers>
+      <Timer />
+    </Timers>
+  );
+}
+
+export default TimerManager;
